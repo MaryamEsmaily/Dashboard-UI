5cf95e22ab8822de2fef0315c86bbce398f229a4
Maryam Esmaili <maryam.esmaily78@gmail.com>
2022-03-19 00:23:35 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const toggle = document.querySelector(".toggle");
+const navigation = document.querySelector(".navigation");
+const main = document.querySelector(".main");
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+});
